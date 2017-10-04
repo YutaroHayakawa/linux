4c6a8f5124b9e8a4f53a88870a0abd80761ab3b8
Yutaro Hayakawa <river@ht.sfc.keio.ac.jp>
2017-10-05 00:08:33 +0900

    Fixed copyright

--- samples/bpf/vale_bpf_loader.c
+++ samples/bpf/vale_bpf_loader.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2016 PLUMgrid
+/* Copyright (c) 2017 Yutaro Hayakawa
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of version 2 of the GNU General Public
